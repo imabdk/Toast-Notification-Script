95701bd3cd571e3f383d6a31a638367152bec27b
Martin Bengtsson <mab@imab.dk>
2020-08-23 09:07:55 +0200

    Uploaded as a part of version 2.0.0

--- New-ToastNotification.ps1
+++ New-ToastNotification.ps1
@@ -79,20 +79,24 @@
 
               
               
-              ** Most of the work done in version 2.0.0 is done by Chad Bower // @Brower_Cha on Twitter **
+              ** Most of the work done in version 2.0.0 is done by Chad Brower // @Brower_Cha on Twitter **
               ** I have added the additional protocols/scripts and rewritten some minor things **
               ** As well as added support for dynamic deadline retrieval for software updates **
               ** Stuff has been rewritten to suit my understanding and thoughts of the script **
 
     2.0.0 -   Huge changes to how this script handles custom protocols
-                - Added Support for Software Updates : Searches for an update (IPU) and will store in variable
                 - Added Support for Custom Actions/Protocols within the script under user context removing the need for that to be run under SYSTEM/ADMIN
                     <Option Name="Action" Value="ToastRunUpdateID:" />
+                    <Option Name="Action" Value="ToastRunPackageID:" />
+                    <Option Name="Action" Value="ToastRunApplicationID:" />
+                    <Option Name="Action" Value="ToastReboot:" />
                 - Added Support to dynamically create Custom Action Scripts to support Custom Protocols
-                - Added New XML Types for SoftwareUpdates : 
-                    <Option Name="RunUpdateTitle" Enabled="True" Value="Version 1909" />
-                    <Option Name="RunUpdateID" Enabled="True" Value="3012973" />
+                - Added Support for Software (Feature) Updates : Searches for an update and will store in variable
+                - Added new XML Types for Software Updates:
+                    - <Option Name="RunUpdateID" Enabled="True" Value="3012973" />
+                    - <Option Name="RunUpdateTitle" Enabled="True" Value="Version 1909" />
                 - Added support for getting deadline date/time dynamically for software updates
+                    - Configure DynamicDeadline with the UpdateID
 
 .LINK
     https://www.imab.dk/windows-10-toast-notification-script/
@@ -243,7 +247,7 @@ function Get-GivenName() {
 	            $RegKey = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\SessionData"
 	            $DisplayName = (Get-ChildItem -Path $RegKey | Where-Object {$_.GetValue("LoggedOnUserSID") -eq $LoggedOnSID} | Select-Object -First 1).GetValue("LoggedOnDisplayName")
 		        if ($DisplayName) {
-                    Write-Log -Message "Given name found in WMI with the CCM client: : $GivenName"
+                    Write-Log -Message "Given name found in WMI with the CCM client: $GivenName"
 			        $GivenName = $DisplayName.Split()[0].Trim()
                     $GivenName
 		        }
